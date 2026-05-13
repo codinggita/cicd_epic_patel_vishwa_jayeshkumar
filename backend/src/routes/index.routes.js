88f007ad7c02f36c8bc246adf62f41e0ebbc374e
Vishwa <vipatel9217@gmail.com>
2026-05-13 23:00:09 +0530

    Add Centralized Routing and Global Middleware Setup

---
+++ backend/src/routes/index.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+
+// v1 route modules will be mounted here as features are added
+// Example: router.use("/auth", require("./v1/authRoutes"));
+
+module.exports = router;
